777eb6ffce319be2ecd81a5590f1a5be95e800ec
gsjoy24 <gour.joy24@gmail.com>
2024-07-24 20:17:04 +0600

    connecting to git

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import config from './app/config';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(
+	cors({
+		origin: ['http://localhost:3000'],
+		credentials: true
+	})
+);
+
+// app.use("/api", router);
+
+app.get('/', (req: Request, res: Response) => {
+	res.send(`Server Running on port ${config.port}`);
+});
+
+// global error handler middleware
+app.use(globalErrorHandler);
+
+// not found middleware
+app.use(notFound);
+
+export default app;
---
+++ src/app/middlewares/notFound.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+const notFound = (req: Request, res: Response, next: NextFunction) => {
+	res.status(httpStatus.NOT_FOUND).json({
+		success: false,
+		message: 'API route not found',
+		error: ''
+	});
+};
+
+export default notFound;
