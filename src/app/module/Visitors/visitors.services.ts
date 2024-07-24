bcfae82e24933584f92e5a9186722e094e4a6aa7
gsjoy24 <gour.joy24@gmail.com>
2024-07-24 22:01:25 +0600

    created service functions to create and get visitors

---
+++ src/app/module/Visitors/visitors.services.ts
@@ -0,0 +1,29 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import Visitor from './visitors.model';
+import { TVisitor } from './visitors.types';
+
+const createVisitor = async (visitor: TVisitor) => {
+	// check if the visitor is already in the database
+	const visitorExists = await Visitor.findOne({
+		email: visitor?.email
+	});
+	if (visitorExists) {
+		throw new AppError(httpStatus.CONFLICT, 'Visitor already exists');
+	}
+
+	const newVisitor = await Visitor.create(visitor);
+	return newVisitor;
+};
+
+const getVisitors = async () => {
+	const visitors = await Visitor.find();
+	return visitors;
+};
+
+const VisitorServices = {
+	createVisitor,
+	getVisitors
+};
+
+export default VisitorServices;
